a3371da5649e6fb8ff608711ed815055beafabc7
shoty-de <66251668+shoty-de@users.noreply.github.com>
2020-11-08 23:52:45 +0900

    Update task.rb
    コールバックの実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,14 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
   validates :name, presence :true
   validates :name, length: { maximum: 30}
   validates :validate_name_not_including_comma
   belongs_to :user
+
+  private
+  
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
+  end
+
 end
