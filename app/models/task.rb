9c421f5a2df7cca216940727935e0146a83373ec
shoty-de <66251668+shoty-de@users.noreply.github.com>
2020-11-12 19:22:50 +0900

    エクスポート

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,14 +1,37 @@
 class Task < ApplicationRecord
+  has_one_attached :image
   before_validation :set_nameless_name
-  validates :name, presence :true
+  # validates :name, presence :true
   validates :name, length: { maximum: 30}
-  validates :validate_name_not_including_comma
+  # validates :validate_name_not_including_comma
   belongs_to :user
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
+  def self.csv_attributes
+    ["name", "description", "created_at", "updated_at"]
+  end
+
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|
+      csv << csv_attributes
+      all.each do |task|
+        csv << csv_attributes.map{|attr| task.send(attr)}
+      end
+    end
+  end
+
+  # scope :recent, -> { order(created_at: :desc) }
 
   private
   
   def set_nameless_name
     self.name = '名前なし' if name.blank?
   end
-
 end
